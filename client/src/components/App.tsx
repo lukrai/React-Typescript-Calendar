4c0e2012789fb9fab904be9fbae13724e8653fe9
lukrai <lukrai007@gmail.com>
2019-06-11 10:46:37 +0300

    Initial React components for Header, Login, Register

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "./Header";
 
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
 const App: React.FC = () => {
     return (
-        <div>
-            <Header/>
-            Text
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/register" component={Register}/>
+                <Route exact path="/login" component={Login}/>
+                <Route exact path="/dashboard" component={Dashboard}/>
+                {/*{props.children}*/}
+            </div>
+        </BrowserRouter>
     );
 };
 
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,56 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
 
-export default class Header extends Component {
+// import * as actions from '../actions';
+
+export default class Header extends Component<any> {
+    constructor(props: any) {
+        super(props);
+        this.signOut = this.signOut.bind(this);
+    }
+
     public render() {
-        return(
-            <div>
-                This is Navbar
-            </div>
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ marginBottom: "30px" }}>
+                <Link className="navbar-brand" to="/">Calendar</Link>
+
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                        </li>
+                    </ul>
+
+                    <ul className="nav navbar-nav ml-auto">
+                        { !this.props.isAuth ?
+                            [<li className="nav-item" key="signup">
+                                <Link className="nav-link" to="/register">Sign Up</Link>
+                            </li>,
+                                <li className="nav-item" key="signin">
+                                    <Link className="nav-link" to="/login">Sign In</Link>
+                                </li>] : null }
+
+                        {/*{ this.props.isAuth ?*/}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/signout" onClick={this.signOut}>Sign Out</Link>
+                            </li>
+                        {/*    </li> : null }*/}
+                    </ul>
+                </div>
+            </nav>
         );
     }
+
+    private signOut() {
+        this.props.signOut();
+    }
 }
+
+function mapStateToProps(state: any) {
+    return {
+        isAuth: state.auth.isAuthenticated,
+    };
+}
+
+// export default connect(mapStateToProps, actions)(Header);
