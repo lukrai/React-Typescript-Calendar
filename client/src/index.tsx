44125b39576e66ab2810cdaa108e0a868c280f80
lukrai <lukrai007@gmail.com>
2019-08-06 00:21:33 +0300

    IE11 cache fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,21 +4,23 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import "react-table/react-table.css";
-import {App} from "./app";
+import { App } from "./app";
 import configureStore from "./common/store";
 import * as serviceWorker from "./serviceWorker";
 
 axios.defaults.withCredentials = true;
+axios.defaults.headers = { Pragma: "no-cache" };
 
 const store = configureStore({});
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <App />
     </Provider>,
-    document.getElementById("root"));
+    document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- server/routes/calendar.routes.ts
+++ server/routes/calendar.routes.ts
@@ -1,6 +1,6 @@
-import { Router} from "express";
+import { Router } from "express";
 import CalendarController from "../controllers/calendar.controller";
-import {authMiddlewareAdmin} from "../middlewares/auth.middleware";
+import { authMiddlewareAdmin } from "../middlewares/auth.middleware";
 
 class CalendarRouter {
     public router: Router;
